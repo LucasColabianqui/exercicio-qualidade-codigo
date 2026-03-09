285aa6c55e99810935486fc77d6bb23e319523b0
Lucas Colabianqui Souza <136926193+LucasColabianqui@users.noreply.github.com>
2026-03-08 21:31:41 -0300

    Add multiplication function with documentation

--- 02-documentacao/bom.js
+++ 02-documentacao/bom.js
@@ -1,1 +1,9 @@
-
+/**
+ * Multiplica dois números
+ * @param {number} numero1
+ * @param {number} numero2
+ * @returns {number}
+ */
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2
+}
