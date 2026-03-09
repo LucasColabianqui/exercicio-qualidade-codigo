02a313616da93d04f531a5ef3e1598301867ab58
Lucas Colabianqui Souza <136926193+LucasColabianqui@users.noreply.github.com>
2026-03-08 21:37:43 -0300

    Add functions to process orders and send emails

--- 05-manutenibilidade/bom.js
+++ 05-manutenibilidade/bom.js
@@ -1,1 +1,12 @@
+function processarPedido(pedido) {
+    calcularValor(pedido)
+    enviarEmail(pedido)
+}
 
+function calcularValor(pedido) {
+    console.log("Calculando valor")
+}
+
+function enviarEmail(pedido) {
+    console.log("Enviando email")
+}
